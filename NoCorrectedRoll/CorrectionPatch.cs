ae7e3df5dc78fe00d57db9777f0a5671825fc577
Antonio Tersigni <tomclancy1995@gmail.com>
2018-05-10 13:43:08 -0400

    Initial commit and code.

---
+++ NoCorrectedRoll/CorrectionPatch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+using BattleTech;
+
+namespace NoCorrectedRoll
+{
+    class CorrectionPatch
+    {
+        [HarmonyPatch(typeof(BattleTech.AttackDirector))]
+        [HarmonyPatch("GetCorrectedRoll")]
+        public static class BattleTech_CorrectedRoll_Prefix
+        {
+            static bool Prefix(AttackDirector __instance, ref float __result, ref float roll, ref Team team)
+            {
+                __result = roll;
+
+                return false; //always override
+            }
+        }
+    }
+}
